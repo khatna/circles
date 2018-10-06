6d8b315e2e01a073992621d2222adf53cd7d31fc
Khatna Bold <bold@princeton.edu>
2018-10-06 03:14:32 +0000

    fix? icon

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app     = express();
 
+var favicon = require('serve-favicon');
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(favicon(__dirname + '/public/imgs/favicon.ico'));
 
 app.get("/", function(req, res) {
 	res.render("circles");	
