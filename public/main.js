14e7bbc9f680b38088930632d2e592974e9dad66
Khatna Bold <bold@princeton.edu>
2018-12-02 03:12:11 +0000

    move away from node

--- public/main.js
+++ public/main.js
@@ -3,157 +3,157 @@ var bandw = false;
 var data = {
 		q: {
 			sound: new Howl({
-	  		src: ['/sounds/bubbles.mp3']
+	  		src: ['public/sounds/bubbles.mp3']
 			}),
 			color: '#1abc9c'
 		},
 		w: {
 			sound: new Howl({
-	  		src: ['/sounds/clay.mp3']
+	  		src: ['public/sounds/clay.mp3']
 			}),
 			color: '#2ecc71'
 		},
 		e: {
 			sound: new Howl({
-	  		src: ['/sounds/confetti.mp3']
+	  		src: ['public/sounds/confetti.mp3']
 			}),
 			color: '#3498db'
 		},
 		r: {
 			sound: new Howl({
-	  		src: ['/sounds/corona.mp3']
+	  		src: ['public/sounds/corona.mp3']
 			}),
 			color: '#9b59b6'
 		},
 			t: {
 			sound: new Howl({
-	  		src: ['/sounds/dotted-spiral.mp3']
+	  		src: ['public/sounds/dotted-spiral.mp3']
 			}),
 			color: '#34495e'
 		},
 		y: {
 			sound: new Howl({
-	  		src: ['/sounds/flash-1.mp3']
+	  		src: ['public/sounds/flash-1.mp3']
 			}),
 			color: '#16a085'
 		},
 		u: {
 			sound: new Howl({
-	  		src: ['/sounds/flash-2.mp3']
+	  		src: ['public/sounds/flash-2.mp3']
 			}),
 			color: '#27ae60'
 		},
 		i: {
 			sound: new Howl({
-	  		src: ['/sounds/flash-3.mp3']
+	  		src: ['public/sounds/flash-3.mp3']
 			}),
 			color: '#2980b9'
 		},
 		o: {
 			sound: new Howl({
-				src: ['/sounds/glimmer.mp3']
+				src: ['public/sounds/glimmer.mp3']
 			}),
 			color: '#8e44ad'
 		},
 		p: {
 			sound: new Howl({
-	  		src: ['/sounds/moon.mp3']
+	  		src: ['public/sounds/moon.mp3']
 			}),
 			color: '#2c3e50'
 		},
 		a: {
 			sound: new Howl({
-	  		src: ['/sounds/pinwheel.mp3']
+	  		src: ['public/sounds/pinwheel.mp3']
 			}),
 			color: '#f1c40f'
 		},
 		s: {
 			sound: new Howl({
-	  		src: ['/sounds/piston-1.mp3']
+	  		src: ['public/sounds/piston-1.mp3']
 			}),
 			color: '#e67e22'
 		},
 			d: {
 			sound: new Howl({
-	  		src: ['/sounds/piston-2.mp3']
+	  		src: ['public/sounds/piston-2.mp3']
 			}),
 			color: '#e74c3c'
 		},
 		f: {
 			sound: new Howl({
-	  		src: ['/sounds/prism-1.mp3']
+	  		src: ['public/sounds/prism-1.mp3']
 			}),
 			color: '#95a5a6'
 		},
 		g: {
 			sound: new Howl({
-	  		src: ['/sounds/prism-2.mp3']
+	  		src: ['public/sounds/prism-2.mp3']
 			}),
 			color: '#f39c12'
 		},
 		h: {
 			sound: new Howl({
-	  		src: ['/sounds/prism-3.mp3']
+	  		src: ['public/sounds/prism-3.mp3']
 			}),
 			color: '#d35400'
 		},
 		j: {
 			sound: new Howl({
-	  		src: ['/sounds/splits.mp3']
+	  		src: ['public/sounds/splits.mp3']
 			}),
 			color: '#1abc9c'
 		},
 		k: {
 			sound: new Howl({
-	  		src: ['/sounds/squiggle.mp3']
+	  		src: ['public/sounds/squiggle.mp3']
 			}),
 			color: '#2ecc71'
 		},
 		l: {
 			sound: new Howl({
-	  		src: ['/sounds/strike.mp3']
+	  		src: ['public/sounds/strike.mp3']
 			}),
 			color: '#3498db'
 		},
 		z: {
 			sound: new Howl({
-	  		src: ['/sounds/suspension.mp3']
+	  		src: ['public/sounds/suspension.mp3']
 			}),
 			color: '#9b59b6'
 		},
 		x: {
 			sound: new Howl({
-	  		src: ['/sounds/timer.mp3']
+	  		src: ['public/sounds/timer.mp3']
 			}),
 			color: '#34495e'
 		},
 		c: {
 			sound: new Howl({
-	  		src: ['/sounds/ufo.mp3']
+	  		src: ['public/sounds/ufo.mp3']
 			}),
 			color: '#16a085'
 		},
 		v: {
 			sound: new Howl({
-	  		src: ['/sounds/veil.mp3']
+	  		src: ['public/sounds/veil.mp3']
 			}),
 			color: '#27ae60'
 		},
 		b: {
 			sound: new Howl({
-	  		src: ['/sounds/wipe.mp3']
+	  		src: ['public/sounds/wipe.mp3']
 			}),
 			color: '#2980b9'
 		},
 		n: {
 			sound: new Howl({
-				src: ['/sounds/zig-zag.mp3']
+				src: ['public/sounds/zig-zag.mp3']
 			}),
 			color: '#8e44ad'
 		},
 		m: {
 			sound: new Howl({
-	  		src: ['/sounds/moon.mp3']
+	  		src: ['public/sounds/moon.mp3']
 			}),
 			color: '#2c3e50'
 		}
